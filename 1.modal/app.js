63b3dc894696dea8b128e81e857ab3532cc5c246
Bhupendra Shahi <shahi23.bhupendra11@gmail.com>
2021-02-01 14:47:20 +0530

    adding projects

---
+++ 1.modal/app.js
@@ -0,0 +1,18 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+let modalBtn = document.querySelector('.modal-btn');
+let modalOverlay = document.querySelector('.modal-overlay');
+let closeBtn = document.querySelector('.close-btn');
+
+//adding event listner for modalBtn
+modalBtn.addEventListener('click', function(){
+    modalOverlay.classList.add('open-modal');
+});
+
+//adding event listner to close
+closeBtn.addEventListener('click', function(){
+    modalOverlay.classList.remove('open-modal');
+});
